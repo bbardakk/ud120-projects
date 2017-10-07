f640d4449f588692f9f21ce41543c0e255d86f41
Ayshine <aysin.tasdelen@hotmail.com>
2017-10-07 20:58:26 +0300

    Committing Outliers mini project

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -17,3 +17,32 @@ data = featureFormat(data_dict, features)
 
 
 
+max_salary = 0
+max_salary_key = None
+
+for key in data_dict:
+	if data_dict[key]["salary"] != 'NaN' and data_dict[key]["salary"] > max_salary:
+		max_salary = data_dict[key]["salary"]
+		max_salary_key = key
+#
+data_dict.pop(max_salary_key, 0)
+print max_salary_key
+# Who made $6-8 mil bonus and over $1 mil salary
+
+for key in data_dict:
+	if data_dict[key]["salary"] != 'NaN' and data_dict[key]["bonus"] != 'NaN':
+		if data_dict[key]["salary"] > 1.0e6 and data_dict[key]["bonus"] > 5.5e6:
+			print key
+
+features = ["salary", "bonus"]
+
+data = featureFormat(data_dict, features)
+
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
+
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
